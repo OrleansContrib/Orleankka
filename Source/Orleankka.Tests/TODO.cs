fd7d34d54f1d5f52c03f3161468fd01d96434364
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-02-04 21:57:47 +0200

    Update TODO

--- Source/Orleankka.Tests/TODO.cs
+++ Source/Orleankka.Tests/TODO.cs
@@ -9,6 +9,13 @@ namespace Orleankka
     public class TodoFixture
     {
         [Test, Ignore]
+        public void LambdaActors()
+        {
+            // - Add support for function actors
+            // - Need to support all avail attributes (passed inside spawn() func)
+        }
+
+        [Test, Ignore]
         public void AutomaticDeactivation()
         {
             // - Add support for per-type idle deactivation timeouts
