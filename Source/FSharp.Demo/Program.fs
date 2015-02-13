e6524bbe055d156b3db2b0dbf14c28c2174f2442
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-02-13 22:36:27 +0200

    Split projects

--- Source/FSharp.Demo/Program.fs
+++ Source/FSharp.Demo/Program.fs
@@ -35,6 +35,8 @@ let main argv =
               |> register [|assembly|]
               |> start
 
+   ActorToActor.startDemo(system).Wait()
+
    // todo: add task builder fsharp
    
 //   let actor = system.ActorOf<Greeter>(Guid.NewGuid().ToString())
