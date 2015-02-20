b4cd6264f45c04eddaff4638dcafe09885b69f2f
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-02-20 18:00:45 +0200

    Update TODO

--- Source/Orleankka.Tests/TODO.cs
+++ Source/Orleankka.Tests/TODO.cs
@@ -9,19 +9,19 @@ namespace Orleankka
     public class TodoFixture
     {
         [Test, Ignore]
+        public void Envelopes()
+        {
+            // - Envelope and Body attributte
+            // - Proper selection of Receive channel based on Body type
+            // - Proper dispatching in TypeActor based on Body type
+        }
+
+        [Test, Ignore]
         public void AutomaticDeactivation()
         {
             // - Add support for per-type idle deactivation timeouts
         }
-        
-        [Test, Ignore]
-        public void Samples()
-        {
-            // - Add DI container sample (Unity)
-            // - Add ProtoBuf/Bond serialization sample
-            // - Add Azure CloudService sample
-        }
-        
+
         [Test, Ignore]
         public void AzureSystem()
         {
@@ -33,12 +33,22 @@ namespace Orleankka
         {
             // - TypedActor with automatic handler wire-up and ability to override dispatch (On, Via)
         }
-        
+
+        [Test, Ignore]
+        public void Samples()
+        {
+            // - Add DI container sample (Unity)
+            // - Add ProtoBuf/Bond serialization sample
+            // - Add Azure CloudService sample
+        }
+
         [Test, Ignore]
         public void Messages()
         {
             // - Require [Message] attribute for all in/out message classes ???
             // - Add support (deep-copy) for mutable (yay) messages
+            // - Support weak (name convention) attribute definition, 
+            //   to remove dependency on Orleankka from message contract lib ???
         }
     }
 }
