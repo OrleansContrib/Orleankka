b03a956533998223dc56b8d7a06e75d007edbcea
AntyaDev <antyadev@gmail.com>
2015-02-23 00:40:51 +0200

    improved FuncActor (added init func) added FuncActorDemo.

--- Source/FSharp.Api/FuncActor.fs
+++ Source/FSharp.Api/FuncActor.fs
@@ -5,11 +5,13 @@ open System.Text
 open System.Reflection
 open System.Collections.Generic
 open System.Threading.Tasks
+
 open Orleans
 open Orleankka
 open Orleankka.FSharp.Task
 open Orleankka.Cluster
 open Orleankka.Embedded
+
 open FSharp.Quotations.Evaluator
 open Nessos.FsPickler
 
@@ -17,16 +19,21 @@ type ActorContext = {
    mutable Response : obj
 }
 
+type private Init = obj -> obj
+type private Receive = obj -> obj -> ActorContext -> Task<obj>
+
 type ActorConfig = {
    Name : string
-   Receive : obj * obj * ActorContext -> Task<obj>
+   Init : Init
+   Receive : Receive
 }
 
 type ActorConfigurationBuilder() =
       
    member this.Zero() = {
-      Name = ""
-      Receive = fun (state, msg, context) -> task { return state }
+      Name = ""     
+      Init = fun state -> state       
+      Receive = fun state msg context -> task { return state }
    }
 
    member this.Yield(()) = this.Zero()
@@ -34,31 +41,37 @@ type ActorConfigurationBuilder() =
    [<CustomOperation("name", MaintainsVariableSpace = true)>]
    member this.Id(config, name) = { config with ActorConfig.Name = name }
 
+   [<CustomOperation("init", MaintainsVariableSpace = true)>]
+   member this.Init(config, init : 'TState -> 'TState) = { 
+      config with Init = fun state -> init(Unchecked.defaultof<'TState>) :> obj }
+
    [<CustomOperation("receive", MaintainsVariableSpace = true)>]
    member this.Receive(config, receive : 'TState -> 'TMessage -> ActorContext -> Task<'TState>) = { 
-      config with Receive = fun (state, msg, context) -> 
+      config with Receive = fun state msg context -> 
                               (receive (state :?> 'TState) (msg :?> 'TMessage) context)
                                  .ContinueWith<obj>(fun (task : Task<'TState>) -> task.Result :> obj)
    }
 
 let actor = ActorConfigurationBuilder()
 
-let private actorHandlers = Dictionary<string, FSharpFunc<obj * obj * ActorContext, Task<obj>>>()   
+let private actorConfigs = Dictionary<string, ActorConfig>()   
 
 type private FuncActor() =
    inherit Actor()
       
    let _context = { Response = null }
    let mutable _state = null
-   let mutable _handler = Unchecked.defaultof<FSharpFunc<obj * obj * ActorContext, Task<obj>>>
+   let mutable _receive = Unchecked.defaultof<Receive>
 
    override this.OnActivate() = 
-      _handler <- actorHandlers.[base.Id.Split([|':'|], count = 2).[0]]
+      let config = actorConfigs.[base.Id.Split([|':'|], count = 2).[0]]
+      _state <- config.Init(_state)
+      _receive <- config.Receive
       TaskDone.Done
 
    override this.OnReceive(msg : obj) = task {
       _context.Response <- null
-      let! newState = _handler.Invoke(_state, msg, _context)
+      let! newState = _receive _state msg _context
       _state <- newState
       return _context.Response
    }
@@ -66,31 +79,30 @@ type private FuncActor() =
 type FuncActorBootstrap() =
    inherit Bootstrapper()
 
-   override this.Run(properties : IDictionary<string, string>) =
-      let handler = properties.Item("test_actor")
+   let compileExpr (handler : string) =
       let bytes = Encoding.Default.GetBytes(handler)
       let binary = FsPickler.CreateBinary()
-      let expr = binary.UnPickle<Quotations.Expr<FSharpFunc<obj * obj * ActorContext, Task<obj>>>>(bytes)
-      let compiled = expr.Compile()
-      actorHandlers.Add("test_actor", compiled)
+      let expr = binary.UnPickle<Quotations.Expr<ActorConfig>>(bytes)
+      expr.Compile()
+
+   override this.Run(properties : IDictionary<string, string>) =      
+      properties |> Seq.filter(fun p -> p.Key <> "<-::Type::->")
+                 |> Seq.iter (fun p -> actorConfigs.Add(p.Key, compileExpr(p.Value)))
       TaskDone.Done
 
-let private serialize (actorHandler : 'TState * 'TMessage * ActorContext -> Task<'TState>) =
+let private serializeConfig (config : ActorConfig) =   
    let binary = FsPickler.CreateBinary()
-   <@ actorHandler @> |> binary.Pickle |> Encoding.Default.GetString
-
-let serializeConfig (config : ActorConfig) =
-   let serializedHandler = config.Receive |> serialize
-   config.Name, serializedHandler
+   let serialized = <@ config @> |> binary.Pickle |> Encoding.Default.GetString
+   config.Name, serialized
 
 let registerFuncActors (configs : ActorConfig seq) (silo : EmbeddedConfigurator) =          
    let dict = Dictionary<string, string>()         
    
    configs |> Seq.map(serializeConfig) 
-            |> Seq.iter(fun (name, handler) -> dict.Add(name, handler))      
+           |> Seq.iter(fun (name, handler) -> dict.Add(name, handler))      
    
    silo |> System.register [|Assembly.GetExecutingAssembly()|]
-         |> System.run<FuncActorBootstrap> dict
+        |> System.run<FuncActorBootstrap> dict
 
 let spawn<'TState, 'TMessage> (system : IActorSystem) (config : ActorConfig) id =   
    system.ActorOf(ActorPath.From(typedefof<FuncActor>, config.Name + ":" + id))
---
+++ Source/FSharp.FuncActorDemo/Shop.fs
@@ -0,0 +1,39 @@
+﻿module Shop
+
+open Orleankka
+open Orleankka.FSharp
+open Orleankka.FSharp.FuncActor
+open Account
+
+type ShopMessage =
+   | Sell of Account : ActorRef * Count : int
+   | CheckIn of Count : int
+   | Cash
+   | Stock
+
+type private ShopState = {
+   Price : int
+   Cash : int
+   Stock : int
+}
+
+let ShopActor = actor {
+   name "Shop"
+   init (fun shop -> { Price = 10; Cash = 0; Stock = 0 })
+   receive (fun shop message context -> task {
+      match message with
+
+      | CheckIn count -> return { shop with Stock = shop.Stock + count }
+      
+      | Sell (account, count) ->
+         let amount = count * shop.Price
+         do! account <? Withdraw(amount)
+         return { shop with Cash = shop.Cash + amount; Stock = shop.Stock - count }         
+      
+      | Cash  -> context.Response <- shop.Cash
+                 return shop
+
+      | Stock -> context.Response <- shop.Stock
+                 return shop
+   })
+}
