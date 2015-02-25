084a59a40504c2a9a6a1d684bf9cad539731a223
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-02-26 01:30:55 +0200

    Version bump

--- Source/Orleankka.TestKit.Version.cs
+++ Source/Orleankka.TestKit.Version.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
