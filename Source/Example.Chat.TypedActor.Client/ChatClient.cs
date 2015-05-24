a508006cd0faa5d7cb08d756136cee95f4d10b1b
Elan Hasson <elan@elanhasson.com>
2015-05-24 10:51:51 -0400

    Initial Issue

---
+++ Source/Example.Chat.TypedActor.Client/ChatClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Example.Chat.TypedActor.Server;
+using Orleankka;
+using Orleankka.Typed;
+
+namespace Example.Chat.TypedActor.Client
+{
+    public class ChatClient
+    {
+        public ChatClient()
+        {
+        }
+
+        public ChatClient(TypedActorRef<ChatServer> server, ObserverRef clientRef, string userName)
+        {
+            Server = server;
+            ClientRef = clientRef;
+            UserName = userName;
+        }
+
+        public TypedActorRef<ChatServer> Server { get; set; }
+        public ObserverRef ClientRef { get; set; }
+        public string UserName { get; set; }
+
+        public void Handle(NewMessage message)
+        {
+            Console.WriteLine("{0}: {1}", message.Username, message.Text);
+        }
+
+        public void Handle(NotificationMessage message)
+        {
+            Console.WriteLine("{0}", message.Text);
+        }
+
+        public async Task Join()
+        {
+            await Server.Call(c => c.Join(UserName, ClientRef));
+        }
+
+        public async Task Disconnect()
+        {
+            await Server.Call(c => c.Disconnect(UserName, ClientRef));
+        }
+
+        public async Task Say(string text)
+        {
+            await Server.Call(c => c.Say(UserName, text));
+        }
+    }
+}
