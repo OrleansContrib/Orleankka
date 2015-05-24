d9e2f26aa099deb30b3e97b1a2e56aece3961e09
Elan Hasson <elan@elanhasson.com>
2015-05-23 22:27:37 -0400

    Corrected Space and Reference Issues

--- Source/FSharp.Demo.Chat.Server/ChatServer.fs
+++ Source/FSharp.Demo.Chat.Server/ChatServer.fs
@@ -1,43 +1,45 @@
-﻿open System
-open System.Reflection
+﻿module ChatServer
 
 open Orleankka
 open Orleankka.FSharp
-open Orleankka.Playground
+open System.Collections.Generic
+open System.Linq
 
-type Message = 
-   | Greet of string
-   | Hi
+type ClientMessage =
+   | NewMessage of Username:string * Text:string
+   | Notification of Text:string
 
-type Greeter() = 
-   inherit Actor<Message>()   
+type ServerMessage =
+   | Join of Username:string * Client:ObserverRef
+   | Say of Username:string * Text:string   
+   | Disconnect of Username:string * Client:ObserverRef
+
+
+type ChatServer() = 
+   inherit Actor<ServerMessage>()
+
+   let _users = Dictionary<string, IObserverCollection>()
+
+   let notifyClients msg = _users.Values |> Seq.iter(fun clients -> clients <* msg)
 
    override this.Receive message reply = task {
       match message with
-      | Greet who -> printfn "Hello %s" who
-      | Hi -> printfn "Hello from F#!"           
+      
+      | Join (userName, client) -> 
+         match _users.TryGetValue(userName) with         
+         | (true, clients) -> clients.Add(client)         
+         | _ -> Notification(sprintf "User: %s connected..." userName) |> notifyClients
+                let clients = ObserverCollection() :> IObserverCollection
+                clients.Add(client)
+                _users.Add(userName, clients)
+                         
+         reply "Hello and welcome to Orleankka chat example"                              
+      
+      | Say (userName, text) -> NewMessage(userName, text) |> notifyClients
+      
+      | Disconnect (userName, client) -> 
+         match _users.TryGetValue(userName) with
+         | (true, clients) -> if clients.Count() = 1 then _users.Remove(userName) |> ignore
+                              else clients.Remove(client)
+         | _ -> ()
    }
-
-[<EntryPoint>]
-let main argv = 
-
-    printfn "Running demo. Booting cluster might take some time ...\n"
-
-    use system = ActorSystem.Configure()
-                            .Playground()
-                            .Register(Assembly.GetExecutingAssembly())
-                            .Done()
-                  
-    let actor = system.ActorOf<Greeter>(Guid.NewGuid().ToString())
-
-    task {
-      do! actor <! Hi
-      do! actor <! Greet "Yevhen"
-      do! actor <! Greet "AntyaDev"
-    } 
-    |> Task.wait
-    
-    Console.ReadLine() |> ignore
-
-    printfn "%A" argv
-    0
