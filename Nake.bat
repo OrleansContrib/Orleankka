6555f9a5a92d777b8fc1460e2c38f5fda0f1e293
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-06-19 23:34:17 +0300

    Update Nake version

--- Nake.bat
+++ Nake.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET DIR=%~dp0.
-SET NAKE_VERSION=2.3.0
+SET NAKE_VERSION=2.3.4
 SET NAKE_RUNNER=%DIR%\Packages\Nake.%NAKE_VERSION%\tools\net45\Nake.exe
 
 IF NOT EXIST "%NAKE_RUNNER%" "%DIR%\Tools\NuGet.exe" install Nake -Version %NAKE_VERSION% -o "%DIR%\Packages" 
