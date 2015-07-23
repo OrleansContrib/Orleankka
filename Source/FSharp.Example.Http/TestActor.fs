fa9aef6009b1cd182080d09870684b4ca30dd467
AntyaDev <antyadev@gmail.com>
2015-07-23 19:07:47 +0200

    small update for HTTP example. (should came up with better format for sending DU)

--- Source/FSharp.Example.Http/TestActor.fs
+++ Source/FSharp.Example.Http/TestActor.fs
@@ -5,6 +5,7 @@ open Orleankka.FSharp
 type HelloMessage =
    | Hi of string
    | WhatIsYourName
+   | GiveMeMoney of currency:string * amount:double
 
 type TestActor() =
    inherit Actor<HelloMessage>()
@@ -13,4 +14,5 @@ type TestActor() =
       match message with
       | Hi name        -> reply("hello " + name + "! I am a TestActor.")
       | WhatIsYourName -> reply("My name is TestActor")
+      | GiveMeMoney (c,a) -> reply(sprintf "You want %f %s ??? Fuck you!!!" a c)
    }
