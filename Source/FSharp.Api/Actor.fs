d13ca091ff1146ae54fb2203d01da6e6276c65b6
antyadev <antyadev@gmail.com>
2016-03-02 22:16:15 +0100

    added support for TypedActors with ability to handle up to 4 type of messages in one actor.

--- Source/FSharp.Api/Actor.fs
+++ Source/FSharp.Api/Actor.fs
@@ -1,11 +1,23 @@
 ﻿namespace Orleankka.FSharp
 
+open System.Threading.Tasks
+open Orleankka
+open Orleankka.FSharp.Task
+
+module ActorRefExtensions =
+
+   type Ref =
+      static member Ask(ref:ActorRef, msg:obj) = ref.Ask<'TResponse>(msg)
+      static member Ask(ref:ActorRef<'TActor>, msg:ActorMessage<'TActor>) = ref.Ask<'TResponse>(msg)     
+      static member Tell(ref:ActorRef, msg:obj) = ref.Ask(msg) |> Task.map(ignore)
+      static member Tell(ref:ActorRef<'TActor>, msg:ActorMessage<'TActor>) = ref.Ask(msg) |> Task.map(ignore)
+      static member Notify(ref:ActorRef, msg:obj) = ref.Notify(msg)
+      static member Notify(ref:ActorRef<'TActor>, msg:ActorMessage<'TActor>) = ref.Notify(msg)
+
 [<AutoOpen>]
 module Actor =
 
-   open System.Threading.Tasks
-   open Orleankka
-   open Orleankka.FSharp.Task
+   open ActorRefExtensions
 
    [<AbstractClass>]
    type Actor<'TMessage>() = 
@@ -15,21 +27,115 @@ module Actor =
       
       let reply result = _response <- result
 
-      abstract Receive: message:'TMessage -> reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage * reply:(obj -> unit) -> Task<unit>
       
-      abstract ReceiveUntyped: message:obj -> reply:(obj -> unit) -> Task<unit>
-      default this.ReceiveUntyped (message:obj) (reply:obj -> unit) = Task.FromResult()
+      abstract ReceiveUntyped: message:obj * reply:(obj -> unit) -> Task<unit>
+      default this.ReceiveUntyped(message:obj, reply:obj -> unit) = Task.FromResult()
 
       override this.OnReceive(message:obj) = task {
          _response <- null
          match message with
-         | :? 'TMessage as m -> do! this.Receive m reply
+         | :? 'TMessage as m -> do! this.Receive(m, reply)
                                 return _response
          
-         | _                 -> do! this.ReceiveUntyped message reply
+         | _                 -> do! this.ReceiveUntyped(message, reply)
                                 return _response
       }
-   
-   let inline (<!) (actorRef:ActorRef) (message:obj) = actorRef.Ask(message) |> Task.map(ignore)
-   let inline (<?) (actorRef:ActorRef) (message:obj) = actorRef.Ask<'TResponse>(message)
-   let inline (<*) (ref:^ref) (message:obj) = (^ref : (member Notify : obj -> unit) (ref, message))
+
+   [<AbstractClass>]
+   type Actor<'TMessage1, 'TMessage2>() = 
+      inherit Actor()
+      
+      let mutable _response = null
+      
+      let reply result = _response <- result
+
+      abstract Receive: message:'TMessage1 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage2 * reply:(obj -> unit) -> Task<unit>
+      
+      abstract ReceiveUntyped: message:obj * reply:(obj -> unit) -> Task<unit>
+      default this.ReceiveUntyped(message:obj, reply:obj -> unit) = Task.FromResult()
+
+      override this.OnReceive(message:obj) = task {
+         _response <- null
+         match message with
+         | :? 'TMessage1 as m -> do! this.Receive(m, reply)
+                                 return _response
+                                           
+         | :? 'TMessage2 as m -> do! this.Receive(m, reply)
+                                 return _response         
+
+         | _                  -> do! this.ReceiveUntyped(message, reply)
+                                 return _response
+      }
+
+   [<AbstractClass>]
+   type Actor<'TMessage1, 'TMessage2, 'TMessage3>() = 
+      inherit Actor()
+      
+      let mutable _response = null
+      
+      let reply result = _response <- result
+
+      abstract Receive: message:'TMessage1 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage2 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage3 * reply:(obj -> unit) -> Task<unit>
+      
+      abstract ReceiveUntyped: message:obj * reply:(obj -> unit) -> Task<unit>
+      default this.ReceiveUntyped(message:obj, reply:obj -> unit) = Task.FromResult()
+
+      override this.OnReceive(message:obj) = task {
+         _response <- null
+         match message with
+         | :? 'TMessage1 as m -> do! this.Receive(m, reply)
+                                 return _response
+                                           
+         | :? 'TMessage2 as m -> do! this.Receive(m, reply)
+                                 return _response
+         
+         | :? 'TMessage3 as m -> do! this.Receive(m, reply)
+                                 return _response         
+
+         | _                  -> do! this.ReceiveUntyped(message, reply)
+                                 return _response
+      }
+
+   [<AbstractClass>]
+   type Actor<'TMessage1, 'TMessage2, 'TMessage3, 'TMessage4>() = 
+      inherit Actor()
+      
+      let mutable _response = null
+      
+      let reply result = _response <- result
+
+      abstract Receive: message:'TMessage1 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage2 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage3 * reply:(obj -> unit) -> Task<unit>
+      abstract Receive: message:'TMessage4 * reply:(obj -> unit) -> Task<unit>
+      
+      abstract ReceiveUntyped: message:obj * reply:(obj -> unit) -> Task<unit>
+      default this.ReceiveUntyped(message:obj, reply:obj -> unit) = Task.FromResult()
+
+      override this.OnReceive(message:obj) = task {
+         _response <- null
+         match message with
+         | :? 'TMessage1 as m -> do! this.Receive(m, reply)
+                                 return _response
+                                           
+         | :? 'TMessage2 as m -> do! this.Receive(m, reply)
+                                 return _response
+         
+         | :? 'TMessage3 as m -> do! this.Receive(m, reply)
+                                 return _response
+         
+         | :? 'TMessage4 as m -> do! this.Receive(m, reply)
+                                 return _response         
+
+         | _                  -> do! this.ReceiveUntyped(message, reply)
+                                 return _response
+      }
+
+      
+   let inline (<?) actorRef message = Ref.Ask(actorRef, message)      
+   let inline (<!) actorRef message = Ref.Tell(actorRef, message)   
+   let inline (<*) actorRef message = Ref.Notify(actorRef, message)
