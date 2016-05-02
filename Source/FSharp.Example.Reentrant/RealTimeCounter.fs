afae1d119df7f07a87d3e1a5b129103e49b486a4
AntyaDev <antyadev@gmail.com>
2016-05-02 17:22:29 +0300

    added FSharp sample of Reentrancy.

---
+++ Source/FSharp.Example.Reentrant/RealTimeCounter.fs
@@ -0,0 +1,37 @@
+﻿module RealTimeCounter
+
+open System
+open Orleankka.FSharp
+
+type Message =
+   | Increment
+   | Decrement
+   | GetCount
+
+type Counter() =
+   inherit Actor<Message>()
+
+   let mutable count = 0
+
+   override this.Receive msg = task {
+      match msg with
+      | Increment -> do! Task.delay(TimeSpan.FromSeconds(5.0)) // write to database a new value, IO bound blocking operation
+                     count <- count + 1
+                     return response()
+      
+      | Decrement -> do! Task.delay(TimeSpan.FromSeconds(5.0)) // write to database a new value, IO bound blocking operation
+                     count <- count - 1
+                     return response()
+      
+      | GetCount -> return response(count) // reentrant operation, is not blocking.
+   }
+
+   // this method will be invoked by Orleankka runtime in order to distinguish message on reentrancy,
+   // basically it's just a filter method.
+   static member private IsReentrant(msg:obj) =
+      match msg with
+      | :? Message as m -> match m with
+                           | GetCount -> true  // here we say that GetCount is reentrant message.
+                           | _        -> false
+      | _ -> false
+
