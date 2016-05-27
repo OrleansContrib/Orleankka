182f3e7e80450efa019dc5341b665c2819c50ce2
AntyaDev <antyadev@gmail.com>
2016-05-27 20:34:22 +0300

    added handling of exception and cancellation for awaitTask function.

--- Source/FSharp.Api/Task.fs
+++ Source/FSharp.Api/Task.fs
@@ -16,7 +16,11 @@ let inline wait (task:Task<_>) = task.Wait()
 
 let inline awaitTask (t:Task) = 
    let tcs = TaskCompletionSource()
-   t.ContinueWith(fun t -> tcs.SetResult()) |> ignore
+   t.ContinueWith(fun t -> 
+      match t.IsFaulted with
+      | false -> if t.IsCanceled then tcs.SetCanceled()
+                 else tcs.SetResult()     
+      | true  -> tcs.SetException(t.Exception.GetBaseException())) |> ignore
    tcs.Task
 
 let inline delay (delay:TimeSpan) = 
