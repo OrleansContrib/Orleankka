821a85fd127b6f263d4d6adce3ba8bb9d6588fd3
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2016-07-15 23:31:03 +0300

    Remove unused declarations

--- Source/Orleankka.Tests/Features/One_off_timers.cs
+++ Source/Orleankka.Tests/Features/One_off_timers.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 using NUnit.Framework;
-
-using Orleankka.Services;
-
 using Orleans;
 
 namespace Orleankka.Features
