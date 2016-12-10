8dcaeec53b058c8216dcb3700cb90c5ec7405799
Alexander Prooks <aprooks@live.ru>
2016-12-10 16:33:29 +0200

    feat: add runtime F# project
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

---
+++ FSharp.Runtime/Library1.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp.Runtime
+open System.Reflection
+open Orleans.Runtime.Configuration
+open Orleankka
+open Orleankka.FSharp
+open Orleankka.Playground
+open Orleankka.Cluster
+type Class1() = 
+    member this.X = "F#"
