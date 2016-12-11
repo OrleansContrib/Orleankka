35363ca8ee75a40341377c8bd05983d93f2348d6
Alexander Prooks <aprooks@live.ru>
2016-12-11 13:57:17 +0200

    feat: add .nuget genaration for F#.Runtime
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

---
+++ Source/Orleankka.FSharp.Runtime/Orleankka.FSharp.Runtime.Version.fs
@@ -0,0 +1,10 @@
+﻿namespace Orleankka
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("Orleankka.FSharp.Runtime")>]
+[<assembly: AssemblyProduct("Orleankka")>]
+[<assembly: AssemblyCopyright("Copyright © Anton Moldovan, Yevhen Bobrov 2015")>]
+
+[<assembly: AssemblyVersion("1.0.0")>]
+()
