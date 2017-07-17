d160dcc48ca51460386d27871ddb32044c560926
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-07-17 16:44:23 +0300

    Disable roundtrip serialization

--- Source/Orleankka.TestKit/SerializationExtensions.cs
+++ Source/Orleankka.TestKit/SerializationExtensions.cs
@@ -4,9 +4,7 @@ namespace Orleankka.TestKit
 {
     static class SerializationExtensions
     {
-        internal static object Roundtrip(this SerializationManager manager, object obj) => 
-            manager != null 
-            ? manager.RoundTripSerializationForTesting(obj) 
-            : obj;
+        internal static object Roundtrip(this SerializationManager manager, object obj) => obj;
+
     }
 }
