7c1e97c8a017d1a45fd6e7bdbfd2c856353014f6
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-07-19 15:41:37 +0300

    Add test for storageprovider attribute

--- Source/Orleankka.Tests/Features/Native_orleans_attributes.cs
+++ Source/Orleankka.Tests/Features/Native_orleans_attributes.cs
@@ -1,9 +1,11 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 using NUnit.Framework;
 
 using Orleans.CodeGeneration;
 using Orleans.Concurrency;
+using Orleans.Providers;
 
 namespace Orleankka.Features
 {
@@ -22,31 +24,46 @@ namespace Orleankka.Features
         [StatelessWorker(4)]
         public class TestParameterizedStatelessWorkerActor : Actor {}
 
+        [StorageProvider]
+        public class TestDefaultStorageProviderActor : Actor {}
+
         [TestFixture]
         [RequiresSilo]
         public class Tests
         {
+            static TAttribute AssertHasCustomAttribute<TActor, TAttribute>() where TActor : Actor where TAttribute : Attribute => 
+                AssertHasCustomAttribute<TAttribute>(ActorType.Of<TActor>().Grain);
+
+            static TAttribute AssertHasCustomAttribute<TAttribute>(Type type) where TAttribute : Attribute
+            {
+                var attribute = type.GetCustomAttribute<TAttribute>();
+                Assert.That(attribute, Is.Not.Null);
+                return attribute;
+            }
+
             [Test]
             public void Version_attribute()
             {
                 var @interface = ActorInterface.Of<TestVersionActor>();
-                var attribute = @interface.Grain.GetCustomAttribute<VersionAttribute>();
-                Assert.That(attribute, Is.Not.Null);
+                var attribute = AssertHasCustomAttribute<VersionAttribute>(@interface.Grain);
                 Assert.That(attribute.Version, Is.EqualTo(22));
             }
 
             [Test]
             public void StatelessWorker_attribute()
             {
-                var type = ActorType.Of<TestDefaultStatelessWorkerActor>();
-                var attribute = type.Grain.GetCustomAttribute<StatelessWorkerAttribute>();
-                Assert.That(attribute, Is.Not.Null);
+                var attribute = AssertHasCustomAttribute<TestDefaultStatelessWorkerActor, StatelessWorkerAttribute>();
                 Assert.That(attribute.MaxLocalWorkers, Is.EqualTo(new StatelessWorkerAttribute().MaxLocalWorkers));
 
-                type = ActorType.Of<TestParameterizedStatelessWorkerActor>();
-                attribute = type.Grain.GetCustomAttribute<StatelessWorkerAttribute>();
-                Assert.That(attribute, Is.Not.Null);
+                attribute = AssertHasCustomAttribute<TestParameterizedStatelessWorkerActor, StatelessWorkerAttribute>();
                 Assert.That(attribute.MaxLocalWorkers, Is.EqualTo(4));
+            }
+
+            [Test]
+            public void StorageProvider_attribute()
+            {
+                var attribute = AssertHasCustomAttribute<TestDefaultStorageProviderActor, StorageProviderAttribute>();
+                Assert.That(attribute.ProviderName, Is.EqualTo(new StorageProviderAttribute().ProviderName));
             }
         }
     }
