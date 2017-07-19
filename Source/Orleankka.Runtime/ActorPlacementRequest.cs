fbd4f75920329be999fe41426974586ad8617971
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-07-19 03:25:46 +0300

    Orleankka-tailored placement argument

---
+++ Source/Orleankka.Runtime/ActorPlacementRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Orleans.Runtime.Placement;
+
+namespace Orleankka
+{
+    using Core;
+
+    public struct ActorPlacementRequest
+    {
+        public static ActorPlacementRequest From(PlacementTarget target)
+        {
+            var @interface = ActorInterface.Of(target.InterfaceId);
+            var mapping = @interface.Mapping;
+
+            var id = target.GrainIdentity.PrimaryKeyString;
+            var path = new ActorPath(@interface.Name, id);
+
+            return new ActorPlacementRequest(path, mapping);
+        }
+
+        public readonly ActorPath Path;
+        public readonly Type CustomInterface;
+        public readonly Type ImplementationClass;
+
+        ActorPlacementRequest(ActorPath path, ActorInterfaceMapping mapping)
+        {
+            Path = path;
+            CustomInterface = mapping.CustomInterface;
+            ImplementationClass = mapping.ImplementationClass;
+        }
+    }
+}
--- Source/Orleankka/Core/ActorInterfaceMapping.cs
+++ Source/Orleankka/Core/ActorInterfaceMapping.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Orleankka.Core
 {
     class ActorInterfaceMapping : IEquatable<ActorInterfaceMapping>
     {
-        public static ActorInterfaceMapping Of(string typeName) => new ActorInterfaceMapping(typeName);
+        public static ActorInterfaceMapping Of(string typeName) => new ActorInterfaceMapping(typeName, null, null);
 
         public static ActorInterfaceMapping Of(Type type)
         {
             var name = ActorTypeName.Of(type);
-            var types = new List<Type> {type};
 
+            Type @interface = null;
             if (type.IsClass)
-            {
-                var @interface = ActorTypeName.CustomInterface(type);
-                if (@interface != null)
-                    types.Add(@interface);                
-            }
+                @interface = ActorTypeName.CustomInterface(type);
 
+            Type @class = null;
             if (type.IsInterface)
             {
                 var classes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()
@@ -31,21 +27,25 @@ namespace Orleankka.Core
                         $"Custom actor interface [{type.FullName}] is implemented by " +
                         $"multiple classes: {string.Join(" ; ", classes.Select(x => x.ToString()))}");
 
-                types.Add(classes[0]);                
+                @class = classes[0];                
             }
 
-            return new ActorInterfaceMapping(name, types.ToArray());
+            return new ActorInterfaceMapping(name, @interface, @class);
         }
 
         public readonly string TypeName;
+        public readonly Type CustomInterface;
+        public readonly Type ImplementationClass;
         public readonly Type[] Types;
         public readonly string Key;
 
-        ActorInterfaceMapping(string typeName, params Type[] types)
+        ActorInterfaceMapping(string typeName, Type @interface, Type @class)
         {
             TypeName = typeName;
-            Types = types;
-            Key = $"{typeName} -> {string.Join(";", types.OrderBy(x => x.AssemblyQualifiedName).Select(x => x.AssemblyQualifiedName))}";
+            CustomInterface = @interface;
+            ImplementationClass = @class;
+            Types = new[]{@interface, @class}.Where(x => x != null).ToArray();
+            Key = $"{typeName} -> {string.Join(";", Types.OrderBy(x => x.AssemblyQualifiedName).Select(x => x.AssemblyQualifiedName))}";
         }
 
         public bool Equals(ActorInterfaceMapping other) => 
