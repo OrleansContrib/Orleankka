f518055731996e345d649f10f726d8055c4e0ec6
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-11-15 22:35:40 +0200

    Use expressjs to serve docs

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(express.static('Docs/_site'))
