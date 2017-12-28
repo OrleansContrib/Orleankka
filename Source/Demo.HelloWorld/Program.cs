a2d36a851e6f71dcf123d31c8371b9c0d6fe299e
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-12-28 21:45:38 +0200

    Add C# hello world sample

---
+++ Source/Demo.HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+using Orleankka;
+using Orleankka.Playground;
+
+using static System.Console;
+
+namespace Demo
+{
+    [Serializable]
+    public class Greet
+    {
+        public string Who { get; set; }
+    }
+
+    public interface IGreeter : IActorGrain {}
+
+    public class Greeter : ActorGrain, IGreeter
+    {
+        void On(Greet msg) => WriteLine($"Hello, {msg.Who}!");
+    }
+
+    public static class Program
+    {   
+        public static void Main()
+        {
+            WriteLine("Running example. Booting cluster might take some time ...\n");
+
+            var system = ActorSystem.Configure()
+                .Playground()
+                .Assemblies(Assembly.GetExecutingAssembly())
+                .Done();
+
+            system.Start().Wait();
+
+            var greeter = system.ActorOf<Greeter>("id");
+            greeter.Tell(new Greet {Who = "world"}).Wait();
+
+            Write("\n\nPress any key to terminate ...");
+            ReadKey(true);
+        }
+    }
+}
