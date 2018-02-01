458e30d7f85a4789169e3fe2d55763edd7c35829
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2018-02-01 22:59:20 +0200

    Fix missing parenthesis

--- Source/Orleankka.Runtime/ActorGrain.cs
+++ Source/Orleankka.Runtime/ActorGrain.cs
@@ -26,14 +26,14 @@ namespace Orleankka
         public ActorRef Self => self ?? (self = System.ActorOf(Path));
 
         /// <inheritdoc />
-        protected ActorGrain(IActorRuntime runtime) 
+        protected ActorGrain(IActorRuntime runtime)
             : this(null, runtime)
-        {}
-        
+        { }
+
         /// <inheritdoc />
-        protected ActorGrain(string id) 
+        protected ActorGrain(string id)
             : this(id, null)
-        {}
+        { }
 
         /// <summary>
         /// Provided only for unit-testing purposes
@@ -47,26 +47,26 @@ namespace Orleankka
 
         public string Id => Path.Id;
 
-        public ActorPath Path           {get; private set;}
-        public IActorRuntime Runtime    {get; private set;}
-        Dispatcher Dispatcher           {get; set;}
-        
-        public IActorSystem System           => Runtime.System;
+        public ActorPath Path { get; private set; }
+        public IActorRuntime Runtime { get; private set; }
+        Dispatcher Dispatcher { get; set; }
+
+        public IActorSystem System => Runtime.System;
         public IActivationService Activation => Runtime.Activation;
-        public IReminderService Reminders    => Runtime.Reminders;
-        public ITimerService Timers          => Runtime.Timers;
+        public IReminderService Reminders => Runtime.Reminders;
+        public ITimerService Timers => Runtime.Timers;
 
         Task<object> IActor.ReceiveAsk(object message) => ReceiveRequest(message);
         Task IActor.ReceiveTell(object message) => ReceiveRequest(message);
         Task IActor.ReceiveNotify(object message) => ReceiveRequest(message);
 
-        internal Task<object> ReceiveRequest(object message) => 
+        internal Task<object> ReceiveRequest(object message) =>
             Actor.Middleware.Receive(this, message, Receive);
 
-        Task IRemindable.ReceiveReminder(string name, TickStatus status) => 
+        Task IRemindable.ReceiveReminder(string name, TickStatus status) =>
             Actor.Middleware.Receive(this, Reminder.Message(name, status), Receive);
 
-        public override Task OnDeactivateAsync() => 
+        public override Task OnDeactivateAsync() =>
             Actor.Middleware.Receive(this, Deactivate.Message, Receive);
 
         public override Task OnActivateAsync()
@@ -105,9 +105,13 @@ namespace Orleankka
         {
             Requires.NotNull(message, nameof(message));
 
-            return Dispatcher.DispatchAsync(this, message, x => 
-                x is LifecycleMessage ? Result(Done) : Result(Unhandled));
-        }
+            return Dispatcher.DispatchAsync(this, message, x =>
+            {
+                if (x is LifecycleMessage)
+                    return Result(Done);
 
+                throw new UnhandledMessageException(this, message);
+            });
+        }
     }
 }
