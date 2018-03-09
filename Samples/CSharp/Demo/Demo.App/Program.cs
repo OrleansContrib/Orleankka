6bd21800c6ba8826dc77933b15f289b8c042221b
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2018-03-09 16:50:06 +0200

    Re-organize project structure similar to upstream

---
+++ Samples/CSharp/Demo/Demo.App/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+using Orleankka;
+using Orleankka.Client;
+using Orleankka.Cluster;
+
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+
+namespace Demo
+{
+    public static class Program
+    {
+        static App app;
+
+        public static async Task Main()
+        {
+            Console.WriteLine("Running demo. Booting cluster might take some time ...\n");
+
+            var account = CloudStorageAccount.DevelopmentStorageAccount;
+            var storage = await TopicStorage.Init(account);
+
+            var host = await new SiloHostBuilder()
+                .UseConfiguration(ClusterConfiguration.LocalhostPrimarySilo())
+                .ConfigureServices(x => x
+                    .AddSingleton(storage))
+                .ConfigureApplicationParts(x => x
+                    .AddApplicationPart(typeof(Api).Assembly)
+                    .WithCodeGeneration())
+                .ConfigureOrleankka()
+                .Start();
+
+            var client = await host.Connect();
+            var system = client.ActorSystem();
+
+            app = new App(system, await system.CreateObservable());
+            app.Run();
+
+            Console.WriteLine("Press Enter to terminate ...");
+            Console.ReadLine();
+
+            client.Dispose();
+            host.Dispose();
+        }
+    }
+}
---
+++ Samples/FSharp/HelloWorld/Program.fs
@@ -0,0 +1,81 @@
+﻿module Demo
+
+open System
+open System.Reflection
+
+open FSharp.Control.Tasks
+open Orleankka
+open Orleankka.FSharp
+open Orleankka.Client
+open Orleankka.Cluster
+open Orleans
+open Orleans.Hosting
+open Orleans.Runtime.Configuration
+
+type GreeterMessage = 
+   | Greet of string
+   | Hi
+
+type IGreeter = 
+   inherit IActorGrain<GreeterMessage>
+
+type Greeter() = 
+   inherit ActorGrain()
+   interface IGreeter
+
+   override this.Receive(message) = task {
+      match message with
+        | :? GreeterMessage as m -> 
+            match m with
+            | Greet who -> printfn "Hello %s" who
+                           return none()
+
+            | Hi        -> printfn "Hello from F#!"
+                           return none()
+
+        |_ -> return unhandled()
+   }
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn "Running demo. Booting cluster might take some time ...\n"
+
+    let sc = ClusterConfiguration.LocalhostPrimarySilo()
+                  
+    sc.AddMemoryStorageProvider()
+    sc.AddMemoryStorageProvider("PubSubStore")
+    sc.AddSimpleMessageStreamProvider("sms")
+
+    let sb = new SiloHostBuilder()
+    sb.UseConfiguration(sc) |> ignore
+    sb.ConfigureApplicationParts(fun x -> x.AddApplicationPart(Assembly.GetExecutingAssembly()).WithCodeGeneration() |> ignore) |> ignore
+    sb.ConfigureOrleankka() |> ignore
+
+    use host = sb.Build()
+    host.StartAsync().Wait()
+
+    let cc = ClientConfiguration.LocalhostSilo()
+    cc.AddSimpleMessageStreamProvider("sms")
+
+    let cb = new ClientBuilder()
+    cb.UseConfiguration(cc) |> ignore
+    cb.ConfigureApplicationParts(fun x -> x.AddApplicationPart(Assembly.GetExecutingAssembly()).WithCodeGeneration() |> ignore) |> ignore
+    cb.ConfigureOrleankka() |> ignore
+
+    use client = cb.Build()
+    client.Connect().Wait()
+   
+    let t = task {
+
+        let system = client.ActorSystem()
+        let actor = ActorSystem.typedActorOf<IGreeter, GreeterMessage>(system, "good-citizen")
+      
+        do! actor <! Hi
+        do! actor <! Greet "Yevhen"
+        do! actor <! Greet "World"      
+    }
+    t.Wait()
+    
+    Console.ReadKey() |> ignore 
+    0
