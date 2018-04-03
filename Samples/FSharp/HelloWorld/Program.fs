799d08b1281f6ef4c5ece866639fad35310f8212
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2018-04-03 16:07:25 +0300

    Don't use Shared in F# HelloWorld

--- Samples/FSharp/HelloWorld/Program.fs
+++ Samples/FSharp/HelloWorld/Program.fs
@@ -1,14 +1,18 @@
 ﻿module Demo
 
 open System
+open System.Net
 open System.Reflection
 
 open FSharp.Control.Tasks
+open Orleans
+open Orleans.Hosting
+open Orleans.Configuration
+open Orleans.Runtime
 open Orleankka
+open Orleankka.Cluster
 open Orleankka.Client
 open Orleankka.FSharp
-open Orleankka.Cluster
-open Orleans.Hosting
 
 type GreeterMessage = 
    | Greet of string
@@ -37,16 +41,33 @@ type Greeter() =
 [<EntryPoint>]
 let main argv = 
 
+    let DemoClusterId = "localhost-demo"
+    let LocalhostSiloPort = 11111
+    let LocalhostGatewayPort = 30000
+    let LocalhostSiloAddress = IPAddress.Loopback
+
     printfn "Running demo. Booting cluster might take some time ...\n"
 
     let sb = new SiloHostBuilder()
-    sb.AddAssembly(Assembly.GetExecutingAssembly())
+    sb.Configure<ClusterOptions>(fun (options:ClusterOptions) -> options.ClusterId <- DemoClusterId) |> ignore
+    sb.UseDevelopmentClustering(fun (options:DevelopmentClusterMembershipOptions) -> options.PrimarySiloEndpoint <- IPEndPoint(LocalhostSiloAddress, LocalhostSiloPort)) |> ignore
+    sb.ConfigureEndpoints(LocalhostSiloAddress, LocalhostSiloPort, LocalhostGatewayPort) |> ignore
+    sb.ConfigureApplicationParts(fun x -> x.AddApplicationPart(Assembly.GetExecutingAssembly()).WithCodeGeneration() |> ignore) |> ignore
     sb.ConfigureOrleankka() |> ignore
+  
+    let cb = new ClientBuilder()
+    cb.Configure<ClusterOptions>(fun (options:ClusterOptions) -> options.ClusterId <- DemoClusterId) |> ignore
+    cb.UseStaticClustering(fun (options:StaticGatewayListProviderOptions) -> options.Gateways.Add(IPEndPoint(LocalhostSiloAddress, LocalhostGatewayPort).ToGatewayUri())) |> ignore
+    cb.ConfigureApplicationParts(fun x -> x.AddApplicationPart(Assembly.GetExecutingAssembly()).WithCodeGeneration() |> ignore) |> ignore
+    cb.ConfigureOrleankka() |> ignore
 
-    use host = sb.Start().Result
-    use client = host.Connect().Result
-   
     let t = task {
+
+        let host = sb.Build()
+        do! host.StartAsync()
+
+        let client = cb.Build();
+        do! client.Connect()
 
         let system = client.ActorSystem()
         let actor = ActorSystem.typedActorOf<IGreeter, GreeterMessage>(system, "good-citizen")
