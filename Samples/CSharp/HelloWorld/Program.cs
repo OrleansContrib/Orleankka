36a00cb39bdeadac19db441759585841d1f4a3ef
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2018-04-03 15:38:32 +0300

    Don't use Shared in C# HelloWorld

--- Samples/CSharp/HelloWorld/Program.cs
+++ Samples/CSharp/HelloWorld/Program.cs
@@ -13,6 +13,11 @@ using static System.Console;
 
 namespace Demo
 {
+    using System.Net;
+
+    using Orleans.Configuration;
+    using Orleans.Runtime;
+
     [Serializable]
     public class Greet
     {
@@ -27,22 +32,40 @@ namespace Demo
     }
 
     public static class Program
-    {   
+    {
+        const string DemoClusterId = "localhost-demo";
+        const int LocalhostSiloPort = 11111;
+        const int LocalhostGatewayPort = 30000;
+        static readonly IPAddress LocalhostSiloAddress = IPAddress.Loopback;
+
         public static async Task Main()
         {
             WriteLine("Running example. Booting cluster might take some time ...\n");
 
-            var host = await new SiloHostBuilder()
+            var host = new SiloHostBuilder()
+                .Configure<ClusterOptions>(options => options.ClusterId = DemoClusterId)
+                .UseDevelopmentClustering(options => options.PrimarySiloEndpoint = new IPEndPoint(LocalhostSiloAddress, LocalhostSiloPort))
+                .ConfigureEndpoints(LocalhostSiloAddress, LocalhostSiloPort, LocalhostGatewayPort)
                 .ConfigureApplicationParts(x => x
                     .AddApplicationPart(Assembly.GetExecutingAssembly())
                     .WithCodeGeneration())
                 .ConfigureOrleankka()
-                .Start();
+                .Build();
 
-            var client = await host.Connect();
-            var system = client.ActorSystem();
-            
-            var greeter = system.ActorOf<IGreeter>("id");
+            await host.StartAsync();
+
+            var client = new ClientBuilder()
+                .Configure<ClusterOptions>(options => options.ClusterId = DemoClusterId)
+                .UseStaticClustering(options => options.Gateways.Add(new IPEndPoint(LocalhostSiloAddress, LocalhostGatewayPort).ToGatewayUri()))
+                .ConfigureApplicationParts(x => x
+                    .AddApplicationPart(Assembly.GetExecutingAssembly())
+                    .WithCodeGeneration())
+                .ConfigureOrleankka()
+                .Build();
+
+            await client.Connect();
+
+            var greeter = client.ActorSystem().ActorOf<IGreeter>("id");
             await greeter.Tell(new Greet {Who = "world"});
 
             Write("\n\nPress any key to terminate ...");
