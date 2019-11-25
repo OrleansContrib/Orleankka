e12efd5b92e4f26a14377e3303fc63467b89c3fc
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2019-11-25 16:26:40 +0200

    Don't set vsinstalldir

--- Docs/build.sh
+++ Docs/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
-export VisualStudioVersion="15.0"
+#export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
+#export VisualStudioVersion="16.0"
 
 docfx docfx.json
