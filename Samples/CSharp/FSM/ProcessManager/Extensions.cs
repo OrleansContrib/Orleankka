533b21cc860216052f2133899a95cc6dec918844
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2019-11-25 21:54:13 +0200

    Fix blazor in FSM sample

--- Samples/CSharp/FSM/ProcessManager/Extensions.cs
+++ Samples/CSharp/FSM/ProcessManager/Extensions.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Orleankka.Services;
+﻿using System.IO;
 
 namespace ProcessManager
 {
     public static class Extensions
     {
-        public static void ScheduleOnce(this ITimerService timers, Func<Task> callback, TimeSpan? due = null) => 
-            timers.Register(callback.Method.Name, due: due ?? TimeSpan.FromMilliseconds(1), callback);
-
         public static void DeleteFileIfExists(this string file)
         {
             if (File.Exists(file))
