b21b3b814a5bc089032e68faf8b20004b935dae6
Nejc Pušnik <nejc.pusnik@nomnio.com>
2020-09-09 13:30:38 +0200

    ActorPath.For now also works with a class that implements IActorGrain as the type parameter

---
+++ Tests/Orleankka.Tests/Testing/TestActor.cs
@@ -0,0 +1,10 @@
+﻿using Orleans;
+
+namespace Orleankka.Testing
+{
+    interface ITestActor : IActorGrain, IGrainWithStringKey
+    { }
+
+    class TestActor : DispatchActorGrain, ITestActor
+    { }
+}
