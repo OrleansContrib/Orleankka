424acca94e779e9c07384de4cdcb73b281571d9f
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2021-09-14 23:25:45 +0300

    Use records in reminder tests

--- Tests/Orleankka.Tests/Features/Using_reminders.cs
+++ Tests/Orleankka.Tests/Features/Using_reminders.cs
@@ -13,37 +13,21 @@ namespace Orleankka.Features
         using Meta;
         using Testing;
 
-        [Serializable]
-        public class SetReminder : Command
-        {
-            public TimeSpan Period;
-        }
-        
-        [Serializable]
-        public class Kill : Command
-        {}
+        public record SetReminder(TimeSpan Period) : Command;
+        public record Kill : Command;
+        public record HasBeenReminded : Query<bool>;
+        public record GetInstanceHashcode : Query<long>;
 
-        [Serializable]
-        public class HasBeenReminded : Query<bool>
-        {}
-
-        [Serializable]
-        public class GetInstanceHashcode : Query<long>
-        {}
-
-        public interface ITestActor : IActorGrain, IGrainWithStringKey
-        {}
-
+        public interface ITestActor : IActorGrain, IGrainWithStringKey {}
         public class TestActor : DispatchActorGrain, ITestActor
         {
             bool reminded;
 
             void On(Reminder _)             => reminded = true;
-            bool On(HasBeenReminded x)      => reminded;
+            bool On(HasBeenReminded _)      => reminded;
             void On(SetReminder x)          => Reminders.Register("test", TimeSpan.Zero, x.Period);
-            void On(Kill x)                 => Activation.DeactivateOnIdle();
-            long On(GetInstanceHashcode x)  => RuntimeHelpers.GetHashCode(this);
-            
+            void On(Kill _)                 => Activation.DeactivateOnIdle();
+            long On(GetInstanceHashcode _)  => RuntimeHelpers.GetHashCode(this);
         }
 
         [TestFixture, RequiresSilo]
@@ -64,7 +48,7 @@ namespace Orleankka.Features
                 var actor = system.FreshActorOf<ITestActor>();
                 var hashcode = await actor.Ask(new GetInstanceHashcode());
 
-                await actor.Tell(new SetReminder {Period = TimeSpan.FromMinutes(1.5)});
+                await actor.Tell(new SetReminder(TimeSpan.FromMinutes(1.5)));
                 await actor.Tell(new Kill());
                 await Task.Delay(TimeSpan.FromMinutes(2.0));
 
