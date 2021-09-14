4deb126f76439100253a0e07d3c93e5cc9002b53
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2021-09-15 01:03:51 +0300

    Add task wrapping result helper

--- Source/Orleankka/ActorMessage.cs
+++ Source/Orleankka/ActorMessage.cs
@@ -50,5 +50,25 @@ namespace Orleankka
         /// <returns>The value passed to <paramref name="result"/> argument</returns>
         public static TResult Result<TActor, TResult>(this ActorMessage<TActor, TResult> message, TResult result) 
             where TActor : IActorGrain, IGrainWithStringKey => result;
+
+        /// <summary>
+        /// Helper method to use when building task responses for strongly typed messages which return results.
+        /// If the type of returned result is changed in message declaration this will help catching it at compile time.
+        /// </summary>
+        /// <example>
+        /// <para>
+        ///     class Query : ActorMessage&lt;MyActor,int&gt;{}
+        /// </para>
+        /// <para>
+        ///     Task&lt;object&gt; On(Query x) => x.TaskResult(42);
+        /// </para>
+        /// </example>
+        /// <param name="message">The strongly typed message</param>
+        /// <param name="result">The result to return</param>
+        /// <typeparam name="TActor">The type of the actor to which this message belongs</typeparam>
+        /// <typeparam name="TResult">The type of the returned result</typeparam>
+        /// <returns>The value passed to <paramref name="result"/> argument wrapped in Task&lt;object&gt;</returns>
+        public static Task<object> TaskResult<TActor, TResult>(this ActorMessage<TActor, TResult> message, TResult result) 
+            where TActor : IActorGrain, IGrainWithStringKey => Task.FromResult<object>(result);
     }
 }
